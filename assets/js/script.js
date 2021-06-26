dc5507ba62f2156580f156a9f79adff2d8a7873e
Derek DeLizo <delizoderek@gmail.com>
2021-06-26 14:29:01 -0700

    Added the setQuestion functino for updating the content-area div with the first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
+let headerEl = document.querySelector("#info-header");
+let contentArea = document.querySelector("#content-area");
 let startBtn = document.querySelector("#start-button");
 let timerEl = document.querySelector("#timer");
 
 let quizDuration = 120;
 
+// 2. When th  e start quiz button is pressed the first question pops-up
+function setQuestion(){
+    let tempAnswers = [".memoryAddress()",".addressAt()", "*myVar","myVar.getAddress()"];
+    contentArea.textContent = "";
+    headerEl.textContent = "How do you access the memory address of a variable in c++?";
+    let listEl = document.createElement("ul");
+    for(let ans of tempAnswers){
+        let itemEl = document.createElement("li");
+        itemEl.textContent = ans;
+        listEl.append(itemEl);
+    }
+    contentArea.append(listEl);
+
+
+
+}
+
+// 1. When the start quiz button is pressed the timer begins
 startBtn.addEventListener("click",function(event){
     event.preventDefault();
+
+    setQuestion();
 
     let quizTimer = setInterval(function(){
         quizDuration--;
@@ -18,8 +40,6 @@ startBtn.addEventListener("click",function(event){
     },1000);
 
 });
-// 1. When the start quiz button is pressed the timer begins
-// 2. When the start quiz button is pressed the first question pops-up
 // 3. The question should have a list of 4 answers that the user can click on
 // 4. when the user selects an answer text, should display at the bottom letting them know if they got it right or wrong
 // 5. The quiz ends either when the user answers all the questions or the timer runs out
