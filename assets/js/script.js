b4b79721df215b93c8ece95a88be8e6986c51bef
Derek DeLizo <delizoderek@gmail.com>
2021-06-28 21:03:14 -0700

    Added render function for the highscores and initials page. Added functionality for loading/storing the scores in local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,20 +44,98 @@ let quizQuestion5 = {
     answer3: "Number",
 }
 
-
 let headerEl = document.querySelector("#info-header");
 let contentArea = document.querySelector("#content-area");
+let statusBox = document.querySelector("#status-box");
 let startBtn = document.querySelector("#start-button");
 let timerEl = document.querySelector("#timer");
 
 let quizDuration = 120;
-let quizQuestions = [quizQuestion1,quizQuestion2,quizQuestion3,quizQuestion4,quizQuestion5];
 let quizIndex = 0;
+let quizScore = 0;
+let quizComplete = false;
+let quizQuestions = [quizQuestion1,quizQuestion2,quizQuestion3,quizQuestion4,quizQuestion5];
+let quizHighscores = [];
+let quizTimer;
+
+// Handles loading highscores from the local storage
+function init(){
+    let loadScores = JSON.parse(localStorage.getItem("scoreboard"));
+    if(loadScores !== null){
+        quizHighscores = loadScores;
+    }
+}
+
+function renderLandingPage(){
+
+}
+
+function renderInitialsScreen(){
+    contentArea.textContent = "";
+    headerEl.textContent = "Enter initials your initials"
+    let initialsInput = document.createElement("input");
+    initialsInput.setAttribute("type","text");
+    initialsInput.setAttribute("id","initials-input");
+    initialsInput.setAttribute("placeholder","Enter your initials");
+    contentArea.append(initialsInput);
+    let submitBtn = document.createElement("button");
+    submitBtn.textContent = "Submit";
+    submitBtn.addEventListener("click",function(event){
+        event.preventDefault();
+        if(initialsInput.value !== ""){
+            quizHighscores.push(`${quizScore} - ${initialsInput.value}`);
+            localStorage.setItem("scoreboard",JSON.stringify(quizHighscores));
+            renderHighscores();
+        } else {
+            alert('Please enter your initials');
+        }
+    });
+    contentArea.append(submitBtn);
+}
+
+function renderHighscores(){
+    let backBtn = document.createElement("button");
+    let clearBtn = document.createElement("button");
+    contentArea.textContent = "";
+    statusBox.textContent = "";
+    headerEl.textContent = "Highscores";
+    let scoreboard = document.createElement("ol");
+    for(let score of quizHighscores){
+        let listEl = document.createElement('li');
+        listEl.textContent = score;
+        scoreboard.append(listEl);
+    }
+    contentArea.append(scoreboard);
+
+    backBtn.textContent = 'Go Back';
+    clearBtn.textContent = 'Clear Highscores';
+    statusBox.append(backBtn);
+    statusBox.append(clearBtn);
+}
+
+function answerSelected(event){
+    event.preventDefault();
+    let target = event.target;
+    if(target.matches("button")){
+        let elId = target.getAttribute("id");
+        if(elId === quizQuestions[quizIndex].correctAnswer){
+            statusBox.textContent = `Correct`;
+            quizScore += 1;
+        } else {
+            statusBox.textContent = `Incorrect`;
+        }
+        if(quizIndex + 1 < quizQuestions.length){
+            quizIndex++;
+            loadQuestion();
+        } else {
+            quizComplete = true;
+        }
+    }
+}
 
 // 2. When th  e start quiz button is pressed the first question pops-up
 function loadQuestion(){
     let currQuestion = quizQuestions[quizIndex];
-    let answerStatus = document.createElement("div");
     headerEl.textContent = currQuestion.question;
     contentArea.textContent = "";
     for(let i = 0; i < currQuestion.numAnswers; i++){
@@ -65,42 +143,29 @@ function loadQuestion(){
         answeBtn.textContent = currQuestion[`answer${i}`];
         answeBtn.setAttribute("id",i);
         // 3. The question should have a list of 4 answers that the user can click on
-        answeBtn.addEventListener("click",function(event){
-            event.preventDefault();
-            let target = event.target;
-            if(target.matches("button")){
-                let elId = target.getAttribute("id");
-                if(elId === currQuestion.correctAnswer){
-                    answerStatus.textContent = `Correct`;
-                } else {
-                    answerStatus.textContent = `Incorrect`;
-                }
-                if(quizIndex + 1 < quizQuestions.length){
-                    quizIndex++;
-                    loadQuestion();
-                }
-            }
-        })
+        answeBtn.addEventListener("click",answerSelected);
         contentArea.append(answeBtn);
     }
-    contentArea.append(answerStatus);
+}
+
+function startQuiz(){
+    quizDuration--;
+    timerEl.textContent = `Timer: ${quizDuration} seconds`
+    if(quizDuration <= 0 || quizComplete){
+        // load initials input
+        //load highscore
+        contentArea.textContent = "Thanks for Playing";
+        timerEl.textContent = "";
+        renderInitialsScreen();
+        clearInterval(quizTimer);
+    }
 }
 
 // 1. When the start quiz button is pressed the timer begins
 startBtn.addEventListener("click",function(event){
     event.preventDefault();
     loadQuestion();
-
-    let quizTimer = setInterval(function(){
-        quizDuration--;
-
-        timerEl.textContent = `Timer: ${quizDuration} seconds`;
-        if(quizDuration <= 0){
-            timerEl.textContent = "";
-            clearInterval(quizTimer);
-        }
-
-    },1000);
+    quizTimer = setInterval(startQuiz,1000);
 
 });
 // 4. when the user selects an answer text, should display at the bottom letting them know if they got it right or wrong
@@ -108,4 +173,5 @@ startBtn.addEventListener("click",function(event){
     // Check if the selected answer is correct
     // If it is then 
 // 5. The quiz ends either when the user answers all the questions or the timer runs out
-// 6. When the user completes the quiz there score is added to the scoreboard.
+// 6. When the quiz completes the user is asked to enter there initials
+init();
